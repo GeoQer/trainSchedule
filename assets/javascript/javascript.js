e411ab6bd46e4c0f0a9bace8145c0592628e1e70
GeoQer <40548603+GeoQer@users.noreply.github.com>
2018-09-18 18:25:27 -0700

    Basic bootstrap layout applied. Starting variables defined.

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,14 @@
+var rowCount = 0;
+var trainName = [];
+var destination = [];
+var frequency = [];
+var nextArrival = [];
+
+'<tr>'
+'<th scope="row">' + rowCount + '</th>'
+'<td>' + trainName[rowCount] + '</td>'
+'<td>' + destination[rowCount] + '</td>'
+'<td>' + frequency[rowCount] + '</td>'
+'<td>' + nextArrival[rowCount] + '</td>'
+'<td>' + '</td>'
+'</tr>'
